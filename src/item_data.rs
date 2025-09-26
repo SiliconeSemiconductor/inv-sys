bcbd35ac06fca77ceea25ed9b0a9cc8db0c45482
SiliconeSemiconductor <timkohler123@protonmail.com>
2025-09-26 08:10:03 +0200

    added some items and trying to use string slice for hashmap key instead of String
    probably doesnt work but i want to try it anyway cus it would make adding items a little bit easier.

--- src/item_data.rs
+++ src/item_data.rs
@@ -3,10 +3,12 @@ use std::collections::HashMap;
 use crate::item_handeler::Item;
 use crate::item_handeler::ItemType;
 
-pub fn build_item_data() -> HashMap<String, Item> {
-    #[rustfmt::skip]let mut item_collection = HashMap::from([
-        (String::from("Apple"), Item::build_item("apple", 0.25, "Has like vitamins n fibers inside.", ItemType::Food, 1, 1)),
-        (String::from("Banana"), Item::build_item("babana", 0.12, "Dont eat to much it has like radiation inside", ItemType::Food, 1, 1))
+pub fn build_item_data() -> HashMap<&str, Item> {
+    #[rustfmt::skip]let item_collection: HashMap<&str, Item> = HashMap::from([
+        ("Apple", Item::build_item("apple", 0.25, "Has like vitamins n fibers inside.", ItemType::Food, 1, 1)),
+        ("Banana", Item::build_item("babana", 0.12, "Dont eat to much it has like radiation inside", ItemType::Food, 1, 1)),
+        ("Wood", Item::build_item("Wood", 1.0, "You can burn it, you can build with it, you can probably even eat it.", ItemType::Recource, 1,2)),
+        ("Iron_Ingot", Item::build_item("Iron_Ingot", 20.0, "made of iron...", ItemType::Recource, 2, 1)),
     ]);
     return item_collection;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod item_data;
 mod item_handeler;
 
 fn main() {
-    item_data::build_item_data();
-    println!("Built item Data")
+    let items: std::collections::HashMap<&str, item_handeler::Item> = item_data::build_item_data();
+    println!("Built item Data");
+    for (k, v) in items {
+        println!("{k}");
+        item_handeler::Item::print_item_extended(&v);
+    }
 }
