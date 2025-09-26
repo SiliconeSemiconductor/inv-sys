69b6a7b720f686d6e2158b40aad809aa53f980ec
SiliconeSemiconductor <timkohler123@protonmail.com>
2025-09-26 11:00:55 +0200

    added item attributes

--- src/item_data.rs
+++ src/item_data.rs
@@ -5,10 +5,12 @@ use crate::item_handeler::ItemType;
 
 pub fn build_item_data() -> HashMap<String, Item> {
     #[rustfmt::skip]let item_collection = HashMap::from([
-        (String::from("Apple"), Item::build_item("apple", 0.25, "Has like vitamins n fibers inside.", ItemType::Food, 1, 1)),
-        (String::from("Banana"), Item::build_item("babana", 0.12, "Dont eat to much it has like radiation inside", ItemType::Food, 1, 1)),
-        (String::from("Wood"), Item::build_item("Wood", 2.5, "You can burn it, you can build with it, you can also probably eat it", ItemType::Recource, 2, 1)),
-        (String::from("Iron Bar"), Item::build_item("Iron Bar", 20.0, "made of iron...", ItemType::Recource, 2, 1)),
+        (String::from("Apple"), Item::build_item("apple", 0.25, "Has like vitamins n fibers inside.", ItemType::Food, 1, 1, None)),
+        (String::from("Banana"), Item::build_item("babana", 0.12, "Dont eat to much it has like radiation inside", ItemType::Food, 1, 1, None)),
+        (String::from("Wood"), Item::build_item("Wood", 2.5, "You can burn it, you can build with it, you can also probably eat it", ItemType::Recource, 2, 1, None)),
+        (String::from("Iron Bar"), Item::build_item("Iron Bar", 20.0, "made of iron...", ItemType::Recource, 2, 1, None)),
+        (String::from("M4 Carbine"), Item::build_item("M4 Carbine", 2.92, "Trusted by the US army since '94.", ItemType::Weapon, 3, 2, None)),
+        (String::from("Ak-104"), Item::build_item("Ak-104", 3.9, "Works no matter what.", ItemType::Weapon, 3, 2, None)),
     ]);
     return item_collection;
 }
